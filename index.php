418acfb1d70998019d52c1217e3c6eea865a5cf0
developer1973 <laravel.laravel1@gmail.com>
2016-05-27 01:43:53 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php
+ 
+// By Temy //
+
+$string = "";
+ 
+if(isset($string)) {
+ 
+echo'Yes';
+ 
+} else {
+ 
+echo'No';
+ 
+}
+
+// By Temy //
+
+?>
+</body>
+</html>
